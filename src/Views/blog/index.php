6bacf96b5985f702232bc02543fc161a7ae6d8ab
Ethan <epaleyron@edenschool.fr>
2024-01-16 10:17:35 +0100

    petit scss

--- src/Views/blog/index.php
+++ src/Views/blog/index.php
@@ -8,12 +8,14 @@ ob_start();
     <?php foreach ($blogs as $blog) { ?>
         <article>
             <img src="/files/<?= $blog->getfile_blog() ?>" alt="<?= $blog->getfile_blog() ?>">
-            <h2>
-                <?= $blog->gettitle_blog() ?>
-            </h2>
-            <time datetime="<?= $blog->getdatetime_blog() ?>">
-                <?= $blog->getdatetime_blog() ?>
-            </time>
+            <div class="title_date">
+                <h2>
+                    <?= $blog->gettitle_blog() ?>
+                </h2>
+                <time datetime="<?= $blog->getdatetime_blog() ?>">
+                    <?= $blog->getdatetime_blog() ?>
+                </time>
+            </div>
             <p>
                 <?= $blog->getcontent_blog() ?>
             </p>
